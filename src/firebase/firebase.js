34e31625ff31b1eef747f9b8a5496a683cb67d09
KingOfCramers <32515581+KingOfCramers@users.noreply.github.com>
2018-11-03 15:24:28 -0400

    changed Firebase SDK

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,6 @@
-import * as firebase from 'firebase'; // * as takes all named exports and puts them as methods on a new variable, called firebase
+import * as firebase from 'firebase/app'; // * as takes all named exports and puts them as methods on a new variable, called firebase
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
    apiKey: process.env.FIREBASE_API_KEY, // these are accessed via webpack.config, which passes them down from node. Node gets them from our dotenv files
